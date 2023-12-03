010a494025061ad18c79bfa0cb14d9e036f002f8
Frank Li <frankla0203@outlook.com>
2023-12-03 01:32:27 -0800

    use janestreet formatting

--- bin/day2p1_better.ml
+++ bin/day2p1_better.ml
@@ -2,35 +2,26 @@ open Core
 open In_channel
 
 let file = "data/day2.full"
-
-let red_max, green_max, blue_max = (12, 13, 14)
-
 let contains s1 s2 = String.is_substring ~substring:s2 s1
 
-let max_value = function
-  | "red" -> red_max
-  | "green" -> green_max
-  | "blue" -> blue_max
-  | _ -> raise (Invalid_argument "empty string")
+let split_clear str char =
+  String.split str ~on:char |> List.filter ~f:(fun s -> not (String.is_empty s))
+;;
 
-let colorize list =
-  List.exists ~f:(fun x -> contains x "red") list
-  && Int.of_string (List.nth_exn list 0) <= red_max
-  || List.exists ~f:(fun x -> contains x "green") list
-     && Int.of_string (List.nth_exn list 0) <= green_max
-  || List.exists ~f:(fun x -> contains x "blue") list
-     && Int.of_string (List.nth_exn list 0) <= blue_max
+let max_value = function
+  | "red" -> 12
+  | "green" -> 13
+  | "blue" -> 14
+  | _ -> raise (Invalid_argument "empty string")
+;;
+
+let check_ball ball = Int.of_string ball.(0) <= max_value ball.(1)
 
 let check list =
-  let balls =
-    String.split list ~on:','
-    |> List.filter ~f:(fun s -> not (String.is_empty s))
-  in
-  List.map balls ~f:(fun s ->
-      String.split s ~on:' '
-      |> List.filter ~f:(fun s -> not (String.is_empty s))
-      |> colorize)
+  let balls = split_clear list ',' in
+  List.map balls ~f:(fun s -> split_clear s ' ' |> Array.of_list |> check_ball)
   |> List.reduce_exn ~f:( && )
+;;
 
 let rec process_lines ic score =
   let line = input_line ic in
@@ -46,7 +37,9 @@ let rec process_lines ic score =
     let ok = List.map game ~f:check |> List.reduce_exn ~f:( && ) in
     let res = if ok then game_num else 0 in
     process_lines ic (res + score)
+;;
 
 let solve () =
   let ic = create file in
   process_lines ic 0 |> Int.to_string |> print_endline
+;;
--- bin/day2p1_naive.ml
+++ bin/day2p1_naive.ml
@@ -2,29 +2,28 @@ open Core
 open In_channel
 
 let file = "data/day2.full"
-
-let red_max, green_max, blue_max = (12, 13, 14)
-
+let red_max, green_max, blue_max = 12, 13, 14
 let contains s1 s2 = String.is_substring ~substring:s2 s1
 
 let colorize list =
-  List.exists ~f:(fun x -> contains x "red") list
-  && Int.of_string (List.nth_exn list 0) <= red_max
-  || List.exists ~f:(fun x -> contains x "green") list
-     && Int.of_string (List.nth_exn list 0) <= green_max
-  || List.exists ~f:(fun x -> contains x "blue") list
-     && Int.of_string (List.nth_exn list 0) <= blue_max
+  (List.exists ~f:(fun x -> contains x "red") list
+   && Int.of_string (List.nth_exn list 0) <= red_max)
+  || (List.exists ~f:(fun x -> contains x "green") list
+      && Int.of_string (List.nth_exn list 0) <= green_max)
+  || (List.exists ~f:(fun x -> contains x "blue") list
+      && Int.of_string (List.nth_exn list 0) <= blue_max)
+;;
 
 let check list =
   let balls =
-    String.split list ~on:','
-    |> List.filter ~f:(fun s -> not (String.is_empty s))
+    String.split list ~on:',' |> List.filter ~f:(fun s -> not (String.is_empty s))
   in
   List.map balls ~f:(fun s ->
-      String.split s ~on:' '
-      |> List.filter ~f:(fun s -> not (String.is_empty s))
-      |> colorize)
+    String.split s ~on:' '
+    |> List.filter ~f:(fun s -> not (String.is_empty s))
+    |> colorize)
   |> List.reduce_exn ~f:( && )
+;;
 
 let solve () =
   let ic = create file in
@@ -38,11 +37,11 @@ let solve () =
     | Some line ->
       let arr = String.split line ~on:':' in
       let game_num =
-        Int.of_string
-          (List.nth_exn (String.split (List.nth_exn arr 0) ~on:' ') 1)
+        Int.of_string (List.nth_exn (String.split (List.nth_exn arr 0) ~on:' ') 1)
       in
       let takes = List.nth_exn arr 1 in
       let game = String.split takes ~on:';' in
       let ok = List.map game ~f:check |> List.reduce_exn ~f:( && ) in
       if ok then total := !total + game_num
   done
+;;
