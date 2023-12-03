fc4e55f3cf72ee24f3cc13227783a19c10bbb6c7
Frank Li <frankla0203@outlook.com>
2023-12-03 00:55:37 -0800

    day 2 part 1 solution without reference

--- bin/main.ml
+++ bin/main.ml
@@ -26,7 +26,7 @@ let check list =
       |> colorize)
   |> List.reduce_exn ~f:( && )
 
-let () =
+let day2part1_naive () =
   let ic = create file in
   let total = ref 0 in
   while true do
@@ -46,3 +46,24 @@ let () =
       let ok = List.map game ~f:check |> List.reduce_exn ~f:( && ) in
       if ok then total := !total + game_num
   done
+
+let rec process_lines ic score =
+  let line = input_line ic in
+  match line with
+  | None -> score
+  | Some line ->
+    let arr = String.split line ~on:':' in
+    let game_num =
+      Int.of_string (List.nth_exn (String.split (List.nth_exn arr 0) ~on:' ') 1)
+    in
+    let takes = List.nth_exn arr 1 in
+    let game = String.split takes ~on:';' in
+    let ok = List.map game ~f:check |> List.reduce_exn ~f:( && ) in
+    let res = if ok then game_num else 0 in
+    process_lines ic (res + score)
+
+let day2part1_new () =
+  let ic = create file in
+  process_lines ic 0 |> Int.to_string |> print_endline
+
+let () = day2part1_new ()
